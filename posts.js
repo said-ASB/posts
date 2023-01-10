3c3765b8fb5955764e8df588562f24f527e4a50e
said-ASB <saad.stro@gmail.com>
2023-01-10 18:03:53 +0100

    finished

--- posts.js
+++ posts.js
@@ -1,17 +1,72 @@
 async function posts (){
     const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
     let  data = await resp.json()
-    mapposts(data)
-    
+    localStorage.setItem("posts", JSON.stringify(data))
+
+
 } 
-
-  posts()
+posts()
+const allpost = JSON.parse(localStorage.getItem("posts") || "[]");
+  
 const post = document.querySelector(".post")
+const selectid = document.querySelector(".selectid")
+const creat = document.querySelector(".creat")
+const close = document.querySelector(".close")
+const modal = document.querySelector(".modal")
 
 
-  const mapposts = (elem)=>{
+let empty =[]
+
+
+  function mapposts (elem){
    const mydata =  elem.map(e=>{
-        return `<div class="postDiv">${e.title}<button>See Post</button></div>`
+        return `<div class="postDiv">${e.title}<a href="newpage.html"><button>See Post</button></div></a>`
     }).join("")
     post.innerHTML = mydata
   }
+
+
+
+  const allId = ()=>{
+    allpost.map(x=>{
+    empty.push(x.userId)
+   })
+   let newone = empty;
+
+   Options = [...new Set(newone)];
+
+   myopt = Options.map(c=>
+    `<option>${c}</option>`
+   )
+   selectid.innerHTML += myopt.join('')
+  }
+  allId()
+
+
+creat.addEventListener('click',()=>{
+  modal.classList.add('showmodal')
+})
+
+
+close.addEventListener('click',()=>{
+  if(modal.classList.contains('showmodal')){
+    modal.classList.remove('showmodal')
+    modal.classList.add('.modal')
+  }
+    })
+
+    selectid.addEventListener("click", () => { theid() });    
+    function  theid(){
+      const idinput = selectid.value
+      if(idinput === 'text'){
+           mapposts(allpost)
+      }else{
+        const filtered = allpost.filter(item=>item.userId == idinput
+               
+        );
+        mapposts(filtered)
+      }
+      
+       }
+
+       
